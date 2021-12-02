ddcd89a27c5230ca7643e8c2f5fdd62dca9cb5ce
Abishek Gomes <69644136+abe17124@users.noreply.github.com>
2021-12-02 12:53:24 -0600

    Added comments

--- Node/Position_Tracking_No-Serial.ino
+++ Node/Position_Tracking_No-Serial.ino
@@ -1,3 +1,11 @@
+//Author: Abishek Gomes
+//Revision: --C
+//Date: 12/2/2021
+//Description: Porch Privateers Node GPS Script, this script can 
+//alert stakeholder that a package has moved if it detects a 2m
+//change from the last recorded position.
+//Developed for the City of Wichita.
+
 #include <lmic.h>
 #include <hal/hal.h>
 #include <SPI.h>
@@ -9,39 +17,47 @@
 
 static unsigned char BatLevel[3];
 
+//Arrays to store datapoints retrieved from GPS
 char latData[10] = {}; //Raw Lattitude Array
 char longData[10] = {}; //Raw Longitude Array
 char speedDataMPH[10] = {}; //Speed MPH
 char speedDataMPS[10] = {}; // Speed mps
 
-double prevLat = 32.2737283;
-double prevLong = -91.2937232;
+//Dummy lattitude and longitude for initialization
+double prevLat = 0;
+double prevLong = 0;
 
+//Dummy speeds for init
 double speedMPH = 0;
 double speedMPS = 0;
 
 double currentLat = 0;
 double currentLong = 0;
 
+//Placeholder var for distance
 unsigned long distanceBetween = 0;
 
+//Boolean to track state of LoRa TX
 static bool sendingPacket = false;
 
+//Raw float recieved from VBAT pin
 float vbatPinRaw = 0; //Raw Float Value empty var
 TinyGPSPlus gps; //GPS Object
 
 //Session credentials from Chirpstack
-//EUI Can be 0 for Chirpstack
+//APPEUI Can be 0 for Chirpstack
 static const u1_t PROGMEM APPEUI[8]= { 0000000000000000 };
 void os_getArtEui (u1_t* buf) { memcpy_P(buf, APPEUI, 8);}
 
+//DEVEUI, Remember this needs to be in LSB (Little-Endian)
 static const u1_t PROGMEM DEVEUI[8]= { 0x8f, 0x4e, 0xeb, 0x37, 0x53, 0x15, 0x9e, 0x40 };
 void os_getDevEui (u1_t* buf) { memcpy_P(buf, DEVEUI, 8);}
 
+//AppKey retrieved from Chirpstack, this can be MSB (Big-Endian)
 static const u1_t PROGMEM APPKEY[16] = { 0x9d,0x74,0xb5,0x06,0xc5,0xd2,0x41,0x8e,0x00,0x86,0x91,0x23,0xd4,0x8f,0x3e,0x0e };
 void os_getDevKey (u1_t* buf) {  memcpy_P(buf, APPKEY, 16);}
 
-//Data packet
+//This is the data packet, where all metrics will be appended to
 static unsigned char mydata[45];
 static osjob_t sendjob;
 
@@ -63,7 +79,7 @@ const lmic_pinmap lmic_pins = {
 
 void onEvent (ev_t ev) {
     switch(ev) {
-        case EV_JOINED:
+        case EV_JOINED: //This event triggers when the node pairs successfully to the chripstack server.
             {
               u4_t netid = 0;
               devaddr_t devaddr = 0;
@@ -74,7 +90,7 @@ void onEvent (ev_t ev) {
             LMIC_setLinkCheckMode(0);
             break;
             
-        case EV_TXCOMPLETE:
+        case EV_TXCOMPLETE: //This triggeres when a transmission has been complete from node to gateway (uplink)
             os_setCallback(&sendjob, setSendingFalse); //Create job to set sending bool to false
             break;
         default:
@@ -84,32 +100,37 @@ void onEvent (ev_t ev) {
     }
 }
 
+//This is a helper function that creates a OS job to set the sendingPacket to true
+//OS job so that it runs in a seperate process and doesn't intervene the main process
 void setSendingTrue(osjob_t* j){
   sendingPacket = true; //Blocking variable, prevents double packaging of payload
 }
 
+//This does the opposite of the above, sets it back to false,
+//Thereby allowing another packet to begin compiling.
 void setSendingFalse(osjob_t* j){
   sendingPacket = false; //Resetting the variable
 }
 
+//This is the OS job that actually transmits our packet 
 void do_send(osjob_t* j){
-    // Check if another job is running, shouldn't because we have blocking jobs, but just in case.
     if (LMIC.opmode & OP_TXRXPEND) {
-        Serial.println(F("OP_TXRXPEND, not sending"));
+        Serial.println(F("OP_TXRXPEND, not sending")); //Checking for an existing TX job.
     } else {
-      // Package payload
+      // Package payload and transmit if all clear
         LMIC_setTxData2(1, mydata, sizeof(mydata)-1, 0);
         //Serial.println(F("Packet queued"));
         Serial.println((char *)mydata);
     }
 }
 
+//This function below handles reading all the metrics from the GT-U7 module
 void readGPS(){
   //prevLat = currentLat; //Store the last recorded lat and long as previous
   //prevLong = currentLong;
   while(Serial1.available())
   {
-    gps.encode(Serial1.read()); //Read GPS Data
+    gps.encode(Serial1.read()); //Read GPS Data form the GPS module
   }
 
   if(gps.location.isUpdated())
@@ -121,6 +142,7 @@ void readGPS(){
     currentLong = gps.location.lng();
   }
   if(gps.speed.isUpdated()){
+    //Get speed is available
     dtostrf(gps.speed.mph(), 6, 6, speedDataMPH);
     dtostrf(gps.speed.mps(), 6, 6, speedDataMPS); //Convert and store speed
     speedMPH = gps.speed.mph();
@@ -128,6 +150,7 @@ void readGPS(){
   }
 }
 
+//This function below reads the battery level from our LiPo battery
 void readBattery(){
   //Read Battery Level
   vbatPinRaw = analogRead(VBATPIN); //Read analog value from A7
@@ -135,6 +158,7 @@ void readBattery(){
   vbatPinRaw *= 3.3;  
   vbatPinRaw /= 1024; // Convert Raw Value to Voltage
   
+  //Basic cases are High, Medium, and Low for simplicity.
   if(vbatPinRaw >= 4.0){ //Cases for different voltages
     strcpy((char *)BatLevel, "HI");
   }
@@ -146,7 +170,7 @@ void readBattery(){
   }
 }
 
-//Module Setup
+//This is where we setup both LoRaWAN, and the GT-U7 GPS Module.
 void setup() {
     delay(2000); //Wait until all modules powered up
     //while (! Serial)
@@ -162,16 +186,21 @@ void setup() {
     LMIC_reset();
 
     LMIC_setLinkCheckMode(0); //Verification for connection
-    LMIC_setDrTxpow(DR_SF7,14); //Set data rate and transmit power, need to use ADR sometime soon, so this will be gone. 
+    LMIC_setDrTxpow(DR_SF7,14); //Setting data rate, to use Spreading Factor 7 (Highest data rate)
     LMIC_selectSubBand(1);
 
-    // Initialize job
+    // Initialize job, this sents the first JoinRequest packet.
     do_send(&sendjob);
 }
+
+//This function handles all of the processes of executing both readBattery and readGPS functions,
+//and compiles the data into the mydata packet in sequential
+//Once again this is added as an OS job so that it doesn't interfere with the main process
 void readMeasurements(osjob_t* j){
     readBattery(); //Get Battery Level
     readGPS(); //Get Lat, Long, and Speeds
 
+    //Setting distance between to be calculated with the recorded values.
     distanceBetween =     
      (unsigned long)TinyGPSPlus::distanceBetween(
       currentLat,
